ca4739c5b13964635f073d6b12455c8874f3ee14
gloverd <glover.d@gmail.com>
2023-04-18 10:09:28 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YwDtamhBRyub4kiFWRWgfbWGOsNgS6qFSOVmilwDxqw/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCWD4Q7bEtKUNKElXFWAxdmKvZe9-l1lCM';
